341e937b5402e5f17cad9a2fdcd594b3c809b785
Aldo <xlight97@gmail.com>
2023-04-23 14:27:42 +0800

    Section - Meet another React Component [Finished]

--- src/App.jsx
+++ src/App.jsx
@@ -17,28 +17,45 @@ const learn = [
   },
 ];
 
+function List() {
+  return (
+    <ul>
+      {learn.map((item) => {
+        return (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url} target="_blank" rel="noreferrer">
+                {item.title}
+              </a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <h1>My Hacker Stories</h1>
+      <Search />
 
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <hr />
 
-      <ul>
-        {learn.map((item) => {
-          return (
-            <li key={item.objectID}>
-              <a href={item.url} target="_blank" rel="noreferrer">
-                {item.title}
-              </a>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </li>
-          );
-        })}
-      </ul>
+      <List />
     </div>
   );
 }
