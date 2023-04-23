66f8343ae0cafa88df4b8a421509490df97504cb
Aldo <xlight97@gmail.com>
2023-04-23 14:08:14 +0800

    Section - Lists in React [Finished]

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,44 @@
-const quote = 'React is AWESOME!';
-const user = {
-  name: 'Aldo',
-  dogs: ['Putih', 'Pupi', 'Bruno', 'Mochi'],
-};
-const { name, dogs } = user;
+const learn = [
+  {
+    title: 'React',
+    url: 'https://react.dev',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>
-        {name} is here and {quote}
-      </h1>
-
-      <ol>
-        {dogs.map((e, i) => (
-          <li key={i}>{e}</li>
-        ))}
-      </ol>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+
+      <ul>
+        {learn.map((item) => {
+          return (
+            <li key={item.objectID}>
+              <a href={item.url} target="_blank" rel="noreferrer">
+                {item.title}
+              </a>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
